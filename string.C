89808510945c987220ec67ea179cfc0cc3685cc4
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-21 16:00:40 +0530

    fchar.C

---
+++ string.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+int char a,b,c;
+printf("\nEnter the character name");
+scanf("%d%d",&a &b);
+for(i=0;i<n;i<0)
+{
+int a=1;
+int b=3;
+c=a>b;
+c=1>3;
+return 0;
+}
+}
+
+
